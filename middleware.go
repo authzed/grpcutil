c418276dbb568bcf695343247a811650145d3991
Evan Cordell <cordell.evan@gmail.com>
2021-09-07 16:01:35 -0400

    add grpc service registration helper that adds default middleware
    Signed-off-by: Evan Cordell <cordell.evan@gmail.com>

--- middleware.go
+++ middleware.go
@@ -3,7 +3,11 @@ package grpcutil
 import (
 	"context"
 
+	"github.com/fullstorydev/grpchan"
+	grpcvalidate "github.com/grpc-ecosystem/go-grpc-middleware/validator"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 // IgnoreAuthMix implements the ServiceAuthFuncOverride interface to ignore any
@@ -21,8 +25,25 @@ type AuthlessHealthServer struct {
 	IgnoreAuthMixin
 }
 
-// NewAuthlessServer returns a new gRPC health server that ignores auth
+// NewAuthlessHealthServer returns a new gRPC health server that ignores auth
 // middleware.
 func NewAuthlessHealthServer() *AuthlessHealthServer {
 	return &AuthlessHealthServer{Server: health.NewServer()}
+}
+
+// SetServicesHealthy sets the service to SERVING
+func (s *AuthlessHealthServer) SetServicesHealthy(svcDesc ...*grpc.ServiceDesc) {
+	for _, d := range svcDesc {
+		s.SetServingStatus(
+			d.ServiceName,
+			healthpb.HealthCheckResponse_SERVING,
+		)
+	}
+}
+
+var defaultUnaryMiddleware = grpcvalidate.UnaryServerInterceptor()
+
+// RegisterService registers a service on the registrar with the default middleware enabled
+func RegisterService(srv grpc.ServiceRegistrar, svcDesc *grpc.ServiceDesc, ss interface{}) {
+	srv.RegisterService(grpchan.InterceptServer(svcDesc, defaultUnaryMiddleware, nil), ss)
 }
